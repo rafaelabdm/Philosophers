a8d599ad2b1c47a7551f10dcee94a214345f90b6
rabustam <rabustam@student.42sp.org.br>
2023-02-17 20:54:55 +0100

    getting to know the gettimeofday function

--- thread_test.c
+++ thread_test.c
@@ -6,7 +6,7 @@
 /*   By: rabustam <rabustam@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 17:43:37 by rabustam          #+#    #+#             */
-/*   Updated: 2023/02/16 18:22:03 by rabustam         ###   ########.fr       */
+/*   Updated: 2023/02/17 19:55:58 by rabustam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,16 @@
 
 void	*routine()
 {
-	struct timeval time;
+	struct timeval start_time;
+	struct timeval finish_time;
 
-	gettimeofday(&time, NULL);
-	printf("seconds : %ld\nmicro seconds : %ld\n", time.tv_sec, time.tv_usec);
+	gettimeofday(&start_time, NULL);
+	sleep(1);
+	gettimeofday(&finish_time, NULL);
+	//1 microsecond == 0,001 milisecond
+	//o tempo que vou printar a mensagem - o tempo que começamos o programa = timestamp
+	printf("miliseconds: %ld\n", ((finish_time.tv_sec * 1000 + finish_time.tv_usec / 1000) - \
+	(start_time.tv_sec * 1000 + start_time.tv_usec / 1000)));
 	printf("TESTE!\n");
 	return (NULL);
 }
